d5a908fc4062cee48aa4400d1ea49c0dba63b360
Emmanuel Nicolet <emmanuel.nicolet@gmail.com>
2019-12-01 17:08:58 +0100

    use successors

--- day01/day01.rs
+++ day01/day01.rs
@@ -1,31 +1,21 @@
 use std::io;
 use std::io::prelude::*;
+use std::iter::successors;
 
 fn main() {
-    let masses: Vec<u32> = io::stdin()
+    let fuel_masses: Vec<u32> = io::stdin()
         .lock()
         .lines()
-        .map(|line| line.unwrap().parse().unwrap())
+        .map(|line| line.unwrap().parse::<u32>().unwrap() / 3 - 2)
         .collect();
 
-    println!(
-        "part 1: {}",
-        masses.iter().map(|mass| mass / 3 - 2).sum::<i32>()
-    );
+    println!("part 1: {}", fuel_masses.iter().sum::<u32>());
 
     println!(
         "part 2: {}",
-        masses
+        fuel_masses
             .iter()
-            .map(|mass| {
-                let mut mass = mass / 3 - 2;
-                let mut sum = 0;
-                while mass > 0 {
-                    sum += mass;
-                    mass = mass / 3 - 2;
-                }
-                sum
-            })
-            .sum::<i32>()
+            .map(|&mass| successors(Some(mass), |&x| (x / 3).checked_sub(2)).sum::<u32>())
+            .sum::<u32>()
     );
 }
