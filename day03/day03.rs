bdc276e48f8c5fad38781d0109cf7327f01aa16a
Emmanuel Nicolet <emmanuel.nicolet@gmail.com>
2019-12-03 09:56:09 +0100

    day03

---
+++ day03/day03.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+use std::io;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+struct Instruction {
+    direction: u8,
+    length: usize,
+}
+
+impl FromStr for Instruction {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let direction = s.as_bytes()[0];
+        let length: usize = s.get(1..).unwrap().parse()?;
+        Ok(Self { direction, length })
+    }
+}
+
+fn walk_path(instructions: Vec<Instruction>) -> HashMap<(i32, i32), usize> {
+    let mut x = 0;
+    let mut y = 0;
+    let mut steps = 0;
+    let mut path_taken = HashMap::new();
+
+    for i in instructions {
+        for _ in 0..i.length {
+            match i.direction as char {
+                'U' => y += 1,
+                'R' => x += 1,
+                'D' => y -= 1,
+                'L' => x -= 1,
+                _ => panic!(format!("invalid direction: {}", i.direction)),
+            }
+
+            steps += 1;
+            path_taken.entry((x, y)).or_insert(steps);
+        }
+    }
+
+    path_taken
+}
+
+fn read_wire() -> HashMap<(i32, i32), usize> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    walk_path(
+        input
+            .trim()
+            .split(',')
+            .map(|p| p.parse().unwrap())
+            .collect(),
+    )
+}
+
+fn main() {
+    let wire1 = read_wire();
+    let wire2 = read_wire();
+    let intersections: HashMap<&(i32, i32), &usize> =
+        wire1.iter().filter(|k| wire2.contains_key(k.0)).collect();
+
+    println!(
+        "part 1: {}",
+        intersections
+            .iter()
+            .map(|(p, _)| p.0.abs() + p.1.abs())
+            .min()
+            .unwrap()
+    );
+
+    println!(
+        "part 2: {}",
+        intersections
+            .iter()
+            .map(|(p, &steps)| return steps + wire2.get(p).unwrap_or(&(usize::max_value() - steps)))
+            .min()
+            .unwrap()
+    );
+}
