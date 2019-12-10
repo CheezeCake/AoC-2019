9d8480d1b247fac3e688bb3da76d1efdb3373fe2
Emmanuel Nicolet <emmanuel.nicolet@gmail.com>
2019-12-10 23:08:34 +0100

    rewrite part 1 + cleanup part 2

--- day10/day10.rs
+++ day10/day10.rs
@@ -3,161 +3,89 @@ use std::collections::HashSet;
 use std::io;
 use std::io::prelude::*;
 
-fn detect(
-    map: &Vec<Vec<char>>,
-    row: i32,
-    col: i32,
-    dx: i32,
-    dy: i32,
-    detected: &mut HashSet<(i32, i32)>,
-) -> bool {
-    let mut found = false;
-    let mut x = col + dx;
-    let mut y = row + dy;
-
-    while x >= 0 && x < map.len() as i32 && y >= 0 && y < map.len() as i32 {
-        if map[y as usize][x as usize] == '#' {
-            if detected.contains(&(x, y)) {
-                return false;
-            } else {
-                found = true;
-                detected.insert((x, y));
-            }
-        }
-
-        x += dx;
-        y += dy;
+fn calc_angle((x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> i32 {
+    let mut theta = (x1 as f32 - x2 as f32).atan2(y1 as f32 - y2 as f32);
+    if theta > 0.0 {
+        theta = (2.0 * std::f32::consts::PI) - theta;
+    } else {
+        theta = -theta;
     }
-
-    found
+    theta *= 10_000.0;
+    theta as i32
 }
 
-fn detectable(map: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
-    let row = row as i32;
-    let col = col as i32;
+fn detectable_asteroids(location: (usize, usize), asteroids: &HashSet<(usize, usize)>) -> usize {
+    let mut detected_angles = HashSet::new();
 
-    let mut count = 0;
-    let mut detected = HashSet::new();
-
-    for x in (0..=col).rev() {
-        let dx = x - col;
-        for y in (0..=row).rev() {
-            let dy = y - row;
-            if dx == 0 && dy == 0 {
-                continue;
-            }
-            if detect(&map, row, col, dx, dy, &mut detected) {
-                // println!("found with {}, {}", dx, dy);
-                count += 1;
-            }
-        }
-        for y in row..map.len() as i32 {
-            let dy = y - row;
-            if dx == 0 && dy == 0 {
-                continue;
-            }
-            if detect(&map, row, col, dx, dy, &mut detected) {
-                // println!("found with {}, {}", dx, dy);
-                count += 1;
-            }
+    for &pos in asteroids {
+        if pos != location {
+            detected_angles.insert(calc_angle(location, pos));
         }
     }
 
-    for x in col..map.len() as i32 {
-        let dx = x - col;
-        for y in (0..=row).rev() {
-            let dy = y - row;
-            if dx == 0 && dy == 0 {
-                continue;
-            }
-            if detect(&map, row, col, dx, dy, &mut detected) {
-                // println!("found with {}, {}", dx, dy);
-                count += 1;
-            }
-        }
-        for y in row..map.len() as i32 {
-            let dy = y - row;
-            if dx == 0 && dy == 0 {
-                continue;
-            }
-            if detect(&map, row, col, dx, dy, &mut detected) {
-                // println!("found with {}, {}", dx, dy);
-                count += 1;
-            }
-        }
-    }
+    detected_angles.len()
+}
 
-    // println!("{}, {} -> {}", col, row, count);
-    count
+fn distance((x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> usize {
+    ((x1 as i32 - x2 as i32).abs() + (y1 as i32 - y2 as i32).abs()) as usize
 }
 
 fn main() {
-    let map: Vec<Vec<char>> = io::stdin()
+    let mut asteroids: HashSet<(usize, usize)> = io::stdin()
         .lock()
         .lines()
-        .map(|line| line.unwrap().chars().collect())
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.unwrap()
+                .chars()
+                .enumerate()
+                .filter(|(_, c)| *c == '#')
+                .map(|(x, _)| (x, y))
+                .collect::<Vec<(usize, usize)>>()
+        })
         .collect();
 
-    let mut asteroids = HashSet::new();
-    let mut best_x = -1;
-    let mut best_y = -1;
-    let mut max_count = 0;
-    for i in 0..map.len() {
-        for j in 0..map[i].len() {
-            if map[i][j] == '#' {
-                asteroids.insert((j as i32, i as i32));
-                let count = detectable(&map, i, j);
-                if count > max_count {
-                    max_count = count;
-                    best_x = j as i32;
-                    best_y = i as i32;
-                }
-            }
-        }
-    }
+    let (best_location, count) = asteroids
+        .iter()
+        .map(|&location| (location, detectable_asteroids(location, &asteroids)))
+        .max_by_key(|(_, detected)| *detected)
+        .expect("no asteroid found");
 
-    println!("part 1: {}", max_count);
+    println!("part 1: {}", count);
+    // println!("part 1: {:?}", best_location);
 
-    asteroids.remove(&(best_x, best_y));
+    asteroids.remove(&best_location);
 
-    let mut shot: Vec<(i32, i32)> = Vec::new();
+    let mut vaporized = 0;
 
     while !asteroids.is_empty() {
-        let mut aya = BTreeMap::new();
+        let mut visible = BTreeMap::new();
 
-        for a in &asteroids {
-            let mut theta = ((best_x - a.0) as f64).atan2((best_y - a.1) as f64);
-            if theta > 0.0 {
-                theta = (2.0 * std::f64::consts::PI) - theta;
-            } else {
-                theta = -theta;
-            }
-            theta *= 10_000.0;
-            let theta = theta as i64;
+        for &ast_pos in &asteroids {
+            let theta = calc_angle(best_location, ast_pos);
 
-            if aya.contains_key(&theta) {
-                let (x, y) = aya.get(&theta).unwrap();
-                if (best_x - a.0).abs() + (best_y - a.1).abs()
-                    < (best_y - y).abs() + (best_x - x).abs()
-                {
-                    aya.insert(theta, *a);
-                }
-            } else {
-                aya.insert(theta, *a);
+            if !visible.contains_key(&theta)
+                || distance(best_location, ast_pos)
+                    < distance(best_location, *visible.get(&theta).unwrap())
+            {
+                visible.insert(theta, ast_pos);
             }
         }
 
         let mut angle = -1;
-        for (k, v) in aya {
+        for (k, v) in visible {
             if k != angle {
                 angle = k;
-                shot.push(v);
                 asteroids.remove(&v);
+                vaporized += 1;
+                if vaporized == 200 {
+                    let (x, y) = v;
+                    println!("part 2: {}", x * 100 + y);
+                    return;
+                }
             }
         }
     }
 
-    let (x, y) = shot[200 - 1];
-    // println!("200th shot: {}, {}", x, y);
-    println!("part 2: {}", x * 100 + y);
+    panic!("less than 200 asteroids were vaporized ({})", vaporized);
 }
