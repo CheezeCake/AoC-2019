a172cbf5e882e09f8e46396255c1364da960c052
Emmanuel Nicolet <emmanuel.nicolet@gmail.com>
2019-12-20 16:27:34 +0100

    day20 part 2

--- day20/day20.rs
+++ day20/day20.rs
@@ -4,7 +4,7 @@ use std::collections::VecDeque;
 use std::io;
 use std::io::prelude::*;
 
-#[derive(Hash, PartialEq, Eq, Debug)]
+#[derive(Hash, PartialEq, Eq, Debug, Copy, Clone)]
 enum Tile {
     Portal((char, char)),
     Wall,
@@ -61,18 +61,28 @@ fn read_map() -> Vec<Vec<Tile>> {
     map
 }
 
-fn build_portal_map(map: &Vec<Vec<Tile>>) -> HashMap<(char, char), Vec<(usize, usize)>> {
-    let mut portal_map = HashMap::new();
+fn build_portal_map(map: &Vec<Vec<Tile>>) -> HashMap<(usize, usize), (usize, usize)> {
+    let mut portals = HashMap::new();
 
     for y in 0..map.len() {
         for x in 0..map[y].len() {
             if let Tile::Portal(portal_pair) = map[y][x] {
-                portal_map
+                portals
                     .entry(portal_pair)
                     .or_insert(Vec::new())
                     .push((x, y));
             }
         }
+    }
+
+    let mut portal_map = HashMap::new();
+    for (_, positions) in portals {
+        if positions.len() == 1 {
+            continue;
+        }
+        assert_eq!(positions.len(), 2);
+        portal_map.insert(positions[0], positions[1]);
+        portal_map.insert(positions[1], positions[0]);
     }
 
     portal_map
@@ -88,11 +98,15 @@ fn find_portal_passage((x, y): (usize, usize), map: &Vec<Vec<Tile>>) -> Option<(
     None
 }
 
+fn outer_portal((x, y): (usize, usize), map: &Vec<Vec<Tile>>) -> bool {
+    y == 1 || x == 1 || y == map.len() - 2 || x == map[y].len() - 2
+}
+
 fn shortest_path(
     start: (usize, usize),
     target: Tile,
     map: &Vec<Vec<Tile>>,
-    portal_map: &HashMap<(char, char), Vec<(usize, usize)>>,
+    portal_map: &HashMap<(usize, usize), (usize, usize)>,
 ) -> Option<usize> {
     let mut q = VecDeque::new();
     let mut discovered = HashSet::new();
@@ -112,17 +126,91 @@ fn shortest_path(
                         q.push_back((pos, n + 1));
                         discovered.insert(pos);
                     }
-                    Tile::Portal(portal) => {
+                    Tile::Portal(_) => {
                         if map[pos.1][pos.0] == target {
                             return Some(n);
                         }
-                        for &portal_pos in portal_map.get(&portal).unwrap() {
-                            if !discovered.contains(&portal_pos) {
-                                let passage = find_portal_passage(portal_pos, map)
+                        if let Some(dst_pos) = portal_map.get(&pos) {
+                            if !discovered.contains(&dst_pos) {
+                                let passage = find_portal_passage(*dst_pos, map)
                                     .expect("portal passage not found");
-                                discovered.insert(portal_pos);
+                                discovered.insert(*dst_pos);
                                 discovered.insert(passage);
                                 q.push_back((passage, n + 1));
+                            }
+                        }
+                    }
+                    _ => (),
+                }
+            }
+        }
+    }
+
+    None
+}
+
+fn shortest_path_recursive(
+    start: (usize, usize),
+    target: Tile,
+    map: &Vec<Vec<Tile>>,
+    portal_map: &HashMap<(usize, usize), (usize, usize)>,
+) -> Option<usize> {
+    let mut q = VecDeque::new();
+    let mut discovered = HashMap::new();
+
+    q.push_back((start, 0, 0));
+    discovered.entry(0).or_insert(HashSet::new()).insert(start);
+
+    while let Some((pos, n, level)) = q.pop_front() {
+        let (x, y) = pos;
+
+        for (dx, dy) in &[(0, -1), (1, 0), (0, 1), (-1, 0)] {
+            let (nx, ny) = (x as i32 + dx, y as i32 + dy);
+
+            if within_bounds(nx, ny, map)
+                && !discovered
+                    .get(&level)
+                    .unwrap()
+                    .contains(&(nx as usize, ny as usize))
+            {
+                let pos = (nx as usize, ny as usize);
+
+                discovered.get_mut(&level).unwrap().insert(pos);
+
+                match map[pos.1][pos.0] {
+                    Tile::Passage => {
+                        q.push_back((pos, n + 1, level));
+                    }
+                    Tile::Portal(_) => {
+                        if level == 0 && map[pos.1][pos.0] == target {
+                            return Some(n);
+                        }
+                        if let Some(dst_pos) = portal_map.get(&pos) {
+                            if outer_portal(pos, map) {
+                                if level > 0
+                                    && !discovered
+                                        .entry(level - 1)
+                                        .or_insert(HashSet::new())
+                                        .contains(&dst_pos)
+                                {
+                                    let passage = find_portal_passage(*dst_pos, map)
+                                        .expect("portal passage not found");
+                                    discovered.get_mut(&(level - 1)).unwrap().insert(*dst_pos);
+                                    discovered.get_mut(&(level - 1)).unwrap().insert(passage);
+                                    q.push_back((passage, n + 1, level - 1));
+                                }
+                            } else {
+                                if !discovered
+                                    .entry(level + 1)
+                                    .or_insert(HashSet::new())
+                                    .contains(&dst_pos)
+                                {
+                                    let passage = find_portal_passage(*dst_pos, map)
+                                        .expect("portal passage not found");
+                                    discovered.get_mut(&(level + 1)).unwrap().insert(*dst_pos);
+                                    discovered.get_mut(&(level + 1)).unwrap().insert(passage);
+                                    q.push_back((passage, n + 1, level + 1));
+                                }
                             }
                         }
                     }
@@ -138,13 +226,23 @@ fn shortest_path(
 fn main() {
     let map = read_map();
     let portal_map = build_portal_map(&map);
-    let aa_portal = portal_map.get(&('A', 'A')).expect("portal AA not found");
-    assert!(aa_portal.len() == 1);
-    let start = find_portal_passage(aa_portal[0], &map).expect("start passage not found");
+    let aa_portal = map
+        .iter()
+        .flatten()
+        .enumerate()
+        .find(|(_, &tile)| Tile::Portal(('A', 'A')) == tile)
+        .map(|(p, _)| (p % map[0].len(), p / map[0].len()))
+        .expect("portal AA not found");
+    let start = find_portal_passage(aa_portal, &map).expect("start passage not found");
 
     println!(
         "part 1: {}",
         shortest_path(start, Tile::Portal(('Z', 'Z')), &map, &portal_map)
+            .expect("could not found a way out of the maze")
+    );
+    println!(
+        "part 2: {}",
+        shortest_path_recursive(start, Tile::Portal(('Z', 'Z')), &map, &portal_map)
             .expect("could not found a way out of the maze")
     );
 }
